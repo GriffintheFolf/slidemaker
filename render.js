bc394ab5447214a86a6c5eb7e3ad22d20a2d8ec1
Thomas Sirack <tsirack@protonmail.com>
2022-06-12 10:58:41 -0600

    Render to a video!

--- render.js
+++ render.js
@@ -21,12 +21,20 @@ function draw_text() {
     console.log("Text colour: " + ctx.fillStyle);
     ctx.textAlign = "center";
     ctx.fillText(text, canvas.width/2, canvas.height/2, canvas.width);
+
+    encode_video();
 }
 
-function encode_video(canvas, slide_length) {
+function encode_video() {
+    var ctx = document.getElementById("preview").getContext("2d");
+    var slide_length = document.getElementById("slidelength").value;
     var encoder = new Whammy.Video(FRAMES_PER_SECOND);
 
-    for(let i = 0; i < (slide_length * FRAMES_PER_SECOND); i++) {
-        encoder.add(canvas);
+    for(let i = 0; i < (slide_length * FRAMES_PER_SECOND + 1); i++) {
+        encoder.add(ctx);
     }
+
+    var output = encoder.compile(false, function(output) {
+        document.getElementById("result").src = URL.createObjectURL(output);
+    });
 }
