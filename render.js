95524cc1278c3d01d2e2e78366cc4a3f6da88b3c
chfour <michaleklitwin@gmail.com>
2022-06-21 22:46:54 +0200

    change out most `let`s to `const`s

--- render.js
+++ render.js
@@ -8,27 +8,27 @@ const FRAMES_PER_SECOND = 5;
 
 // ctx.textBaseline must be set to "bottom" for this to work
 function text_height(ctx, text) {
-    let metrics = ctx.measureText(text);
+    const metrics = ctx.measureText(text);
     return metrics.actualBoundingBoxAscent;
 }
 
 function draw_text() {
-    let width = parseInt(document.getElementById("slidewidth").value);
-    let height = parseInt(document.getElementById("slideheight").value);
+    const width = parseInt(document.getElementById("slidewidth").value);
+    const height = parseInt(document.getElementById("slideheight").value);
 
     if (!width || width < 1 || !height || height < 1) return;
 
-    let canvas = document.getElementById("preview");
+    const canvas = document.getElementById("preview");
 
     canvas.setAttribute("width", width);
     canvas.setAttribute("height", height);
 
-    let ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
     ctx.save();
 
-    let bgcolour = document.getElementById("bgcolour").value;
-    let textcolour = document.getElementById("textcolour").value;
-    let text = document.getElementById("textinput").value;
+    const bgcolour = document.getElementById("bgcolour").value;
+    const textcolour = document.getElementById("textcolour").value;
+    const text = document.getElementById("textinput").value;
 
     let offset = 0.0;
 
@@ -38,7 +38,7 @@ function draw_text() {
 
     //var offset = 0.0;
     console.log("Offset: " + offset);
-    let lines = text.split("\n");
+    const lines = text.split("\n");
 
     // calculate initial y offset for text
     for(let i = 0; i < lines.length; i++) {
@@ -63,16 +63,16 @@ function draw_text() {
 }
 
 function encode_video() {
-    let canvas = document.getElementById("preview");
-    let slide_length = document.getElementById("slidelength").value;
-    let frames = [];
+    const canvas = document.getElementById("preview");
+    const slide_length = document.getElementById("slidelength").value;
+    const frames = [];
 
-    let image = canvas.toDataURL("image/webp");
+    const image = canvas.toDataURL("image/webp");
 
     for(let i = 0; i < (slide_length * FRAMES_PER_SECOND + 1); i++) {
         frames.push(image);
     }
 
-    let output = index.fromImageArray(frames, FRAMES_PER_SECOND, false);
+    const output = index.fromImageArray(frames, FRAMES_PER_SECOND, false);
     document.getElementById("result").src = URL.createObjectURL(output);
 }
